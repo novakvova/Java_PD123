869a6caae4c83b6274f446f9fb1455d2302dd0ef
novakvova <novakvova@gmail>
2023-08-30 15:33:39 +0300

    Is good working app

--- _3.SpringBoot/src/main/java/org/example/controllers/CategoryController.java
+++ _3.SpringBoot/src/main/java/org/example/controllers/CategoryController.java
@@ -3,6 +3,7 @@ package org.example.controllers;
 import lombok.AllArgsConstructor;
 import org.example.dto.category.CategoryCreateDTO;
 import org.example.dto.category.CategoryItemDTO;
+import org.example.dto.category.CategoryUpdateDTO;
 import org.example.entities.CategoryEntity;
 import org.example.mappers.CategoryMapper;
 import org.example.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -38,5 +40,41 @@ public class CategoryController {
                 .build();
         categoryRepository.save(cat);
         return cat;
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<CategoryItemDTO> getCategoryById(@PathVariable int id) {
+        Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
+        return categoryOptional
+                .map(category -> ResponseEntity.ok().body(categoryMapper.categoryToItemDTO(category)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(value = "/category/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CategoryItemDTO> updateCategory(@PathVariable int id, @ModelAttribute CategoryUpdateDTO dto) {
+        Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
+        if (categoryOptional.get().getImage() != null && !dto.getImage().isEmpty()) {
+            storageService.removeFile(categoryOptional.get().getImage());
+        }
+        String fileName = storageService.saveMultipartFile(dto.getImage());
+        return categoryOptional.map(category -> {
+            category.setName(dto.getName());
+            category.setDescription(dto.getDescription());
+            category.setImage(fileName);
+            categoryRepository.save(category);
+            return ResponseEntity.ok().body(categoryMapper.categoryToItemDTO(category));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id) {
+        Optional<CategoryEntity> categoryOptional = categoryRepository.findById(id);
+        if (categoryOptional.isPresent()) {
+            storageService.removeFile(categoryOptional.get().getImage());
+            categoryRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
