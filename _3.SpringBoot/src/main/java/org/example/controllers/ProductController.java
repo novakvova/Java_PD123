931bec1cc95193f7acaf548fa95004b7c90964d3
novakvova <novakvova@gmail>
2023-09-04 16:15:48 +0300

    Working product mapper

---
+++ _3.SpringBoot/src/main/java/org/example/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package org.example.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/products")
+public class ProductController {
+
+
+}
